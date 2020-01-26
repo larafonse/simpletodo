cd7e0a82dfcdc4b52e0715f15339d5d9a2ba7136
Nicolas Lara <nicolaslara@Nicolass-MBP.attlocal.net>
2020-01-25 20:35:01 -0800

    able to add itemsto list

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -34,8 +35,20 @@ public class MainActivity extends AppCompatActivity {
         items.add("Have fun!");
 
 
-        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        final ItemsAdapter itemsAdapter = new ItemsAdapter(items);
         rvItems.setAdapter(itemsAdapter);
         rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String todoItem = etItem.getText().toString();
+
+                items.add(todoItem);
+
+                itemsAdapter.notifyItemInserted(items.size() - 1);
+                etItem.setText("");
+            }
+        });
     }
 }
